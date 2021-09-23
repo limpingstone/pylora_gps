8da69625616de90f5ff80f310ab5c72390fe407c
Steven Nyeo <limpingstone@gmail.com>
2021-09-23 06:46:59 -0400

    <GPS> Close serial connection on keyboard interrupt

--- gps_serial.py
+++ gps_serial.py
@@ -1,11 +1,11 @@
 import serial
 
-print('starting serial at 9600 baud...')
+print('Starting serial at 9600 baud...')
 ser = serial.Serial(
     port='/dev/serial0', \
     baudrate=9600)
 
-print('updating serial configuration to 115200 baud...')
+print('Updating serial configuration to 115200 baud...')
 ser.write(b'$PMTK251,115200*1F\r\n')
 
 ser.close()
@@ -13,16 +13,20 @@ ser = serial.Serial(
     port='/dev/serial0', \
     baudrate=115200)
 
-print('updating NMEA frequency and fix CTL to 5 Hz...')
+print('Updating NMEA frequency and fix CTL to 5 Hz...')
 ser.write(b'$PMTK220,200*2C\r\n')
 ser.write(b'$PMTK300,200,0,0,0,0*2F\r\n')
 
 
-while True:
-    line = ser.readline()
-    line_pos = line.decode('ASCII').split(',')
-    for pos in line_pos:
-        print(pos)
+try: 
+    while True:
+        line = ser.readline()
+        line_pos = line.decode('ASCII').split(',')
+        for pos in line_pos:
+            print(pos)
 
-ser.close()
+except KeyboardInterrupt:
+    print('\nClosing serial connection...')
+    ser.close()
+    print('Exited!')
 
