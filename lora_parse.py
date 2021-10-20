d53e1cd14c4ac82aedbb61264530c60acd92964e
Steven Nyeo <limpingstone@gmail.com>
2021-10-20 06:25:40 -0400

    <LoRa> Add string parser for LoRa

---
+++ lora_parse.py
@@ -0,0 +1,15 @@
+
+def str_to_byte(input_str):
+    byte_str = []
+    for char in list(input_str):
+        byte_str.append(ord(char)
+
+    return byte_str
+
+def byte_to_str(input_byte):
+    output_str = ""
+    for byte in byte_str: 
+        output_str += chr(byte)
+
+    return output_str
+
